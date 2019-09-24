a2675743aed14f2acf90c320ad5b5e622b532dcd
Somia Sohail Ansari <ansarisomia@gmail.com>
2019-09-24 12:49:56 +0500

    added component
    email body

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import email from './components/email';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <h1>This is a practice class</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <email 
+        letter="R" 
+        name="reenaishtiaq" 
+        subj="Re: [reenaishtiaq/githu..."
+        text="Merged #1 into master. You a..."/>
+      
+      <email 
+        letter="T" 
+        name="Talha Hussain" 
+        subj="Seriously.." 
+        text="Hello please help me i am a stud..."/>
+      
+      <email 
+        letter="T" 
+        name="me, Tuba 2" 
+        subj="Leave Application" 
+        text="Get well soon. On Thu, Sep 19,2..."/>
+      
+      <email 
+        letter="K" 
+        name="Khansa Saeed" 
+        subj="WaiCamp Confirmation + Remi..." 
+        text="Dear Participant! We are happy t..."/>
+      
+      <email 
+        letter="K" 
+        name="Khansa, me 2" 
+        subj="***Pending WaiCamp Confirmation" 
+        text="Dear Miss, Thanks for your email..."/>
     </div>
   );
 }
---
+++ src/components/email.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './email.css';
+
+const email = (props) => {
+    return (
+        <div className="emailBody">
+            <span>{props.letter}</span><div className="bodyPart"><h4>{props.name}</h4><p>{props.subj}</p><p>{props.text}</p></div>
+        </div>
+    );
+};
+
+export default email;
